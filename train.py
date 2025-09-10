22707cb90e5d1039c1867d6b9ba062aa90264ed6
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-09-10 13:45:12 -0500

    Added validation after each epoch

--- train.py
+++ train.py
@@ -81,6 +81,11 @@ def mixup_data(x, y):
 
 
 
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+
 @app.function(image=image, gpu="A10G", volumes={"/data": volume, "/models": model_volume}, timeout=60 * 60 * 3)
 def train():
     esc50_dir = Path("/opt/esc50-data")
@@ -147,9 +152,50 @@ def train():
         epoch_loss = 0.0
 
         progress_bar = tqdm(
-                train_dataloader, desc=f'Epoch {epoch+1}/{num_epochs}')
+            train_dataloader, desc=f'Epoch {epoch+1}/{num_epochs}')
         for data, target in progress_bar:
+            data, target = data.to(device), target.to(device)
+
+            if np.random.random() > 0.7:
+                data, target_a, target_b, lam = mixup_data(data, target)
+                output = model(data)
+                loss = mixup_criterion(
+                    criterion, output, target_a, target_b, lam)
+            else:
+                output = model(data)
+                loss = criterion(output, target)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            epoch_loss += loss.item()
+            progress_bar.set_postfix({'Loss': f'{loss.item():.4f}'})
+
+        
+        avg_epoch_loss = epoch_loss / len(train_dataloader)
+
+        # Validation after each epoch
+        model.eval()
+
+        correct = 0
+        total = 0
+        val_loss = 0
+
+        with torch.no_grad():
+            for data, target in test_dataloader:
                 data, target = data.to(device), target.to(device)
+                outputs = model(data)
+                loss = criterion(outputs, target)
+                val_loss += loss.item()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+
+        accuracy = 100 * correct / total
+        avg_val_loss = val_loss / len(test_dataloader)
 
 
 @app.local_entrypoint()
